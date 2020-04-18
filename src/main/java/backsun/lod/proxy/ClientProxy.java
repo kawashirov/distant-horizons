06fd1397591502883a74f67e4ae1d1f2a2642933
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 23:10:40 -0500

    have the client listen for world draws

--- src/main/java/backsun/lod/proxy/ClientProxy.java
+++ src/main/java/backsun/lod/proxy/ClientProxy.java
@@ -1,8 +1,12 @@
 package backsun.lod.proxy;
 
+import backsun.lod.util.CustomRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends CommonProxy
 {
@@ -15,5 +19,11 @@ public class ClientProxy extends CommonProxy
 	public void registerItemRender(Item item, int meta, String id)
 	{
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));;
+	}
+	
+	@SubscribeEvent
+	public void renderWorldLastEvent(RenderWorldLastEvent event)
+	{
+		CustomRenderer.drawTest(Minecraft.getMinecraft(),event.getPartialTicks());
 	}
 }
