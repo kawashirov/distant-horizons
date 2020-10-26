3bc0c3368c1fe58ab5d4ac7a8a8fee2871479965
James Seibel <jeseibel@gondtc.com>
2020-10-25 19:57:45 -0500

    Improve variable names

--- src/main/java/backsun/lod/objects/LodRegion.java
+++ src/main/java/backsun/lod/objects/LodRegion.java
@@ -7,16 +7,18 @@ package backsun.lod.objects;
  * one file in the file system.
  * 
  * @author James Seibel
- * @version 09-28-2020
+ * @version 10-22-2020
  */
 public class LodRegion
 {
+	public static final int SIZE = 32;
+	
 	/**	X coordinate of this region */
 	public final int x;
 	/** Z coordinate of this region */
 	public final int z;
 	
-	public LodChunk data[][];
+	public LodChunk chunks[][];
 	
 	
 	public LodRegion(int regionX, int regionZ)
@@ -24,6 +26,6 @@ public class LodRegion
 		x = regionX;
 		z = regionZ;
 		
-		data = new LodChunk[32][32];
+		chunks = new LodChunk[SIZE][SIZE];
 	}
 }
