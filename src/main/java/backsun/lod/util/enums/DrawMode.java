9da15baf697041079e7ca42743960019ea604cf3
James Seibel <jeseibel@gondtc.com>
2021-01-25 18:16:40 -0600

    Add an enum for render modes
    Batch or individual

---
+++ src/main/java/backsun/lod/util/enums/DrawMode.java
@@ -0,0 +1,32 @@
+package backsun.lod.util.enums;
+
+/**
+ * @author James Seibel
+ * @version 1-23-2021
+ */
+public enum DrawMode
+{
+	/** Draw the LOD objects in groups.
+	 * <br>
+	 * <br>
+	 * Fancy fog: render the center and outside LOD
+	 * objects in 2 different groups.
+	 * <br>
+	 * Fast fog: render all LOD objects at one time. 
+	 */
+	BATCH(0),
+	
+	/** Draw each LOD objects separately.
+	 * <br>
+	 * <br>
+	 * Not suggested normally since draw calls are GPU expensive.
+	 */
+	INDIVIDUAL(5);
+	
+	public final int index;
+	
+	private DrawMode(int newValue)
+	{
+		index = newValue;
+	}
+}
