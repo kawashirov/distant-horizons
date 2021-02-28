807818e078bb6dbeae0a8885ef9be763fa5ae15b
James Seibel <jeseibel@gondtc.com>
2021-02-27 21:45:12 -0600

    Fix a few meta info files

--- src/main/java/com/backsun/lod/ModInfo.java
+++ src/main/java/com/backsun/lod/ModInfo.java
@@ -11,6 +11,5 @@ public final class ModInfo
 	public static final String MODID = "lod";
 	public static final String MODNAME = "Levels of Detail";
 	public static final String MODAPI = "LodAPI";
-	public static final String VERSION = "${version}";
-	public static final String APIVERSION = "${apiversion}";
+	public static final String VERSION = "1.0";
 }
