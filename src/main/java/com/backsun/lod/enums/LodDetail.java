a3357c119368ed1ed08e410c337aaaf7e33e20ec
James Seibel <jeseibel@gondtc.com>
2021-05-29 12:45:19 -0500

    Replace LodGeometryQuality with LodDetail

---
+++ src/main/java/com/backsun/lod/enums/LodDetail.java
@@ -0,0 +1,25 @@
+package com.backsun.lod.enums;
+
+/**
+ * single, quad
+ * 
+ * @author James Seibel
+ * @version 05-29-2021
+ */
+public enum LodDetail
+{
+	/** render 1 LOD for each chunk */
+	SINGLE(1),
+	
+	/** render 4 LODs for each chunk */
+	QUAD(4);
+	
+	/** How many data points wide the related
+	 * LodChunk object should contain */
+	public final int value;
+	
+	private LodDetail(int newValue)
+	{
+		value = newValue;
+	}
+}
