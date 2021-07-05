4aebf36f02e7d674dae1afd96a2dc6bd318a2c8d
James Seibel <jeseibel@gondtc.com>
2021-07-05 12:55:42 -0500

    Put the project under the GNU GPL v3 License

--- src/main/java/com/seibel/lod/builders/LodBufferBuilder.java
+++ src/main/java/com/seibel/lod/builders/LodBufferBuilder.java
@@ -1,4 +1,22 @@
+/*
+ *    This file is part of the LOD Mod, licensed under the GNU GPL v3 License.
+ *
+ *    Copyright (C) 2020  James Seibel
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, version 3.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.seibel.lod.builders;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
--- src/main/java/com/seibel/lod/objects/LodDataPoint.java
+++ src/main/java/com/seibel/lod/objects/LodDataPoint.java
@@ -1,3 +1,20 @@
+/*
+ *    This file is part of the LOD Mod, licensed under the GNU GPL v3 License.
+ *
+ *    Copyright (C) 2020  James Seibel
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, version 3.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.seibel.lod.objects;
 
 import java.awt.Color;
