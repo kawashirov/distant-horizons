736b5ba6258da2a1e30831e8416722ff6c115111
Leonardo <leoleo97@libero.it>
2021-08-19 12:47:54 +0200

    new enum for render distance and quality of lods

---
+++ src/main/java/com/seibel/lod/enums/LodQuality.java
@@ -0,0 +1,50 @@
+/*
+ *    This file is part of the LOD Mod, licensed under the GNU GPL v3 License.
+ *
+ *    Copyright (C) 2020  James Seibel
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, version 3.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.seibel.lod.enums;
+
+/**
+ * NONE <br>
+ * BIOME_ONLY <br>
+ * BIOME_ONLY_SIMULATE_HEIGHT <br>
+ * SURFACE <br>
+ * FEATURES <br>
+ * SERVER <br><br>
+ * 
+ * In order of fastest to slowest.
+ * 
+ * @author James Seibel
+ * @author Leonardo Amato
+ * @version 8-7-2021
+ */
+public enum LodQuality
+{
+	LOW(1),
+
+	MEDIUM(2),
+
+	HIGH(3);
+
+
+	/** The higher the number the more complete the generation is. */
+	public final int quality;
+
+	LodQuality(int quality)
+	{
+		this.quality = quality;
+	}
+}
