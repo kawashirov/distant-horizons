1843f96526b682c1bbb63ea0ef021085bbf99b34
Leonardo <leoleo97@libero.it>
2021-09-04 12:48:22 +0200

    various fix to the new classes

--- src/main/java/com/seibel/lod/objects/PosToRenderContainer.java
+++ src/main/java/com/seibel/lod/objects/PosToRenderContainer.java
@@ -1,5 +1,7 @@
 package com.seibel.lod.objects;
 
+import com.seibel.lod.util.LodUtil;
+
 import java.util.Arrays;
 
 public class PosToRenderContainer
@@ -7,6 +9,7 @@ public class PosToRenderContainer
 	private byte minDetail;
 	private int numberOfPosToRender;
 	private int[][] posToRender;
+	/*TODO this population matrix could be converted to boolean to improve memory use*/
 	private byte[][] population;
 
 	public PosToRenderContainer(byte minDetail)
@@ -14,7 +17,8 @@ public class PosToRenderContainer
 		this.minDetail = minDetail;
 		this.numberOfPosToRender = 0;
 		posToRender = new int[1][4];
-		population = new byte[minDetail][minDetail];
+		int size = 1 << (LodUtil.REGION_DETAIL_LEVEL - minDetail);
+		population = new byte[size][size];
 	}
 
 	public void addPosToRender(int[] levelPos)
@@ -23,13 +27,14 @@ public class PosToRenderContainer
 			posToRender = Arrays.copyOf(posToRender, posToRender.length*2);
 		posToRender[numberOfPosToRender] = levelPos;
 		numberOfPosToRender++;
-		int[] newLevelPos = LevelPosUtil.convert(levelPos, minDetail);
+		int[] newLevelPos = LevelPosUtil.getRegionModule(LevelPosUtil.convert(levelPos, minDetail));
 		population[LevelPosUtil.getPosZ(newLevelPos)][LevelPosUtil.getPosZ(newLevelPos)] = (byte) (LevelPosUtil.getDetailLevel(levelPos) + 1);
 	}
 
-	public boolean isToRender(int[] levelPos)
+	public boolean contains(int[] levelPos)
 	{
-		return (population[LevelPosUtil.getPosZ(levelPos)][LevelPosUtil.getPosZ(levelPos)] == (LevelPosUtil.getDetailLevel(levelPos) + 1));
+		int[] newLevelPos = LevelPosUtil.convert(LevelPosUtil.getRegionModule(levelPos), minDetail);
+		return (population[LevelPosUtil.getPosZ(newLevelPos)][LevelPosUtil.getPosZ(newLevelPos)] == (LevelPosUtil.getDetailLevel(levelPos) + 1));
 	}
 
 	public int getNumberOfPos()
@@ -40,5 +45,22 @@ public class PosToRenderContainer
 	public int[] getNthPos(int n)
 	{
 		return posToRender[n];
+	}
+
+	public String toString()
+	{
+
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < numberOfPosToRender; i++)
+		{
+			builder.append(posToRender[i][0]);
+			builder.append(" ");
+			builder.append(posToRender[i][1]);
+			builder.append(" ");
+			builder.append(posToRender[i][2]);
+			builder.append('\n');
+		}
+		builder.append('\n');
+		return builder.toString();
 	}
 }
