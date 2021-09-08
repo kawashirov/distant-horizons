6a1f448b41ed060820de3606937d763326e83e4e
James Seibel <jeseibel@gondtc.com>
2021-09-08 18:13:06 -0500

    comment out testing code

--- src/main/java/com/seibel/lod/objects/LevelPosUtil.java
+++ src/main/java/com/seibel/lod/objects/LevelPosUtil.java
@@ -231,13 +231,13 @@ public class LevelPosUtil
 		int compareResult = Integer.compare(
 				secondDetail,
 				firstDetail);
-		System.out.println("comparing level "+ firstDetail + " " + secondDetail + " " + compareResult);
+//		System.out.println("comparing level "+ firstDetail + " " + secondDetail + " " + compareResult);
 		if (compareResult == 0)
 		{
 			compareResult = compareDistance(
 					firstDistance,
 					secondDistance);
-			System.out.println("Equal level "+ firstDistance + " " + secondDistance + " " + compareResult);
+//			System.out.println("Equal level "+ firstDistance + " " + secondDistance + " " + compareResult);
 		}
 		return compareResult;
 	}
