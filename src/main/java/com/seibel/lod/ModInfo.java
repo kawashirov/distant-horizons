ff43118976a218c405e12c480daa8a70846a6616
James Seibel <jeseibel@gondtc.com>
2021-09-24 18:06:57 -0500

    Update the version number to a1.5.0-pre

--- src/main/java/com/seibel/lod/ModInfo.java
+++ src/main/java/com/seibel/lod/ModInfo.java
@@ -28,5 +28,5 @@ public final class ModInfo
 	public static final String MODID = "lod";
 	public static final String MODNAME = "LOD";
 	public static final String MODAPI = "LodAPI";
-	public static final String VERSION = "a1.4";
+	public static final String VERSION = "a1.5.0-pre";
 }
