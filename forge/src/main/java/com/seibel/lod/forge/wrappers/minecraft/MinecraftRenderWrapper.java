012312892a077f4286d0aa613ad339146fb88f74
Ran <43445785+RanCraftPlayz@users.noreply.github.com>
2021-12-01 10:01:49 +0600

    Whoops, I forgor 💀

--- forge/src/main/java/com/seibel/lod/forge/wrappers/minecraft/MinecraftRenderWrapper.java
+++ forge/src/main/java/com/seibel/lod/forge/wrappers/minecraft/MinecraftRenderWrapper.java
@@ -1,5 +1,6 @@
 package com.seibel.lod.forge.wrappers.minecraft;
 
+import java.awt.*;
 import java.util.HashSet;
 
 import com.mojang.math.Vector3f;
@@ -22,24 +23,25 @@ import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher.CompiledChunk;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.phys.Vec3;
+import org.lwjgl.opengl.GL15;
 
 
 /**
  * A singleton that contains everything
  * related to rendering in Minecraft.
- * 
+ *
  * @author James Seibel
  * @version 11-18-2021
  */
 public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 {
 	public static final MinecraftRenderWrapper INSTANCE = new MinecraftRenderWrapper();
-	
+
 	private final GameRenderer gameRenderer = Minecraft.getInstance().gameRenderer;
 	private final static Minecraft mc = Minecraft.getInstance();
-	
-	
-	
+
+
+
 	@Override
 	public Vec3f getLookAtVector()
 	{
@@ -47,7 +49,7 @@ public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 		Vector3f cameraDir = camera.getLookVector();
 		return new Vec3f(cameraDir.x(), cameraDir.y(), cameraDir.z());
 	}
-	
+
 	@Override
 	public AbstractBlockPosWrapper getCameraBlockPosition()
 	{
@@ -55,58 +57,81 @@ public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 		BlockPos blockPos = camera.getBlockPosition();
 		return new BlockPosWrapper(blockPos.getX(), blockPos.getY(), blockPos.getZ());
 	}
-	
+
 	@Override
 	public boolean playerHasBlindnessEffect()
 	{
 		return mc.player.getActiveEffectsMap().get(MobEffects.BLINDNESS) != null;
 	}
-	
+
 	@Override
 	public Vec3d getCameraExactPosition()
 	{
 		Camera camera = gameRenderer.getMainCamera();
 		Vec3 projectedView = camera.getPosition();
-		
+
 		return new Vec3d(projectedView.x, projectedView.y, projectedView.z);
 	}
-	
+
 	@Override
 	public Mat4f getDefaultProjectionMatrix(float partialTicks)
 	{
 		return McObjectConverter.Convert(gameRenderer.getProjectionMatrix(gameRenderer.getFov(gameRenderer.getMainCamera(), partialTicks, true)));
 	}
-	
+
 	@Override
 	public double getGamma()
 	{
 		return mc.options.gamma;
 	}
-	
+
+	@Override
+	public Color getFogColor()
+	{
+		float[] colorValues = new float[4];
+		GL15.glGetFloatv(GL15.GL_FOG_COLOR, colorValues);
+		return new Color(colorValues[0], colorValues[1], colorValues[2], colorValues[3]);
+	}
+
+	@Override
+	public Color getSkyColor()
+	{
+		if (mc.level.dimensionType().hasSkyLight())
+		{
+			Vec3 colorValues = mc.level.getSkyColor(mc.gameRenderer.getMainCamera().getPosition(), mc.getFrameTime());
+			return new Color((float) colorValues.x, (float) colorValues.y, (float) colorValues.z);
+		}
+		else
+		{
+			return new Color(0, 0, 0);
+		}
+	}
+
+
 	@Override
 	public double getFov(float partialTicks)
 	{
 		return gameRenderer.getFov(gameRenderer.getMainCamera(), partialTicks, true);
 	}
-	
+
 	/** Measured in chunks */
 	@Override
 	public int getRenderDistance()
 	{
 		return mc.options.renderDistance;
 	}
-	
+
 	@Override
 	public int getScreenWidth()
 	{
-		return mc.getWindow().getWidth();	
+		return mc.getWindow().getWidth();
 	}
 	@Override
 	public int getScreenHeight()
 	{
 		return mc.getWindow().getHeight();
 	}
-	
+
 	/**
 	 * This method returns the ChunkPos of all chunks that Minecraft
 	 * is going to render this frame. <br><br>
@@ -119,9 +144,9 @@ public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 	public HashSet<AbstractChunkPosWrapper> getRenderedChunks()
 	{
 		HashSet<AbstractChunkPosWrapper> loadedPos = new HashSet<>();
-		
+
 		// Wow, those are some long names!
-		
+
 		// go through every RenderInfo to get the compiled chunks
 		LevelRenderer renderer = mc.levelRenderer;
 		for (RenderChunkInfo worldRenderer$LocalRenderInformationContainer : renderer.renderChunks)
@@ -131,11 +156,11 @@ public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 			{
 				// add the ChunkPos for every rendered chunk
 				BlockPos bpos = worldRenderer$LocalRenderInformationContainer.chunk.getOrigin();
-				
+
 				loadedPos.add(new ChunkPosWrapper(bpos));
 			}
 		}
-		
+
 		return loadedPos;
 	}
 }
