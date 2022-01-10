12b7723017b42c9d6efddc9a4eb732d161b5b834
coolGi2007 <sasanaps@hotmail.com>
2022-01-10 05:12:22 +0000

    Added cloud warning

--- common/src/main/java/com/seibel/lod/common/Config.java
+++ common/src/main/java/com/seibel/lod/common/Config.java
@@ -129,6 +129,9 @@ public class Config extends ConfigGui
 
 			public static class CloudQuality
 			{
+				@ConfigAnnotations.Comment
+				public static ConfigAnnotations.Comment cloudWarning;
+
 				@ConfigAnnotations.Entry
 				public static boolean customClouds = ICloudQuality.CUSTOM_CLOUDS_DEFAULT;
 
