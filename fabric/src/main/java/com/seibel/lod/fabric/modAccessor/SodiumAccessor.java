e80ccd38fe92847b3e7aa14a7fe9c8dd39b91684
tom lee <tomlee92502@yahoo.com>
2022-01-10 22:05:11 +0800

    Fixed up SodiumAccessor

--- fabric/src/main/java/com/seibel/lod/fabric/modAccessor/SodiumAccessor.java
+++ fabric/src/main/java/com/seibel/lod/fabric/modAccessor/SodiumAccessor.java
@@ -4,42 +4,33 @@ import java.util.HashSet;
 import java.util.stream.Collectors;
 
 import com.seibel.lod.core.util.SingletonHandler;
-import com.seibel.lod.core.wrapperInterfaces.IWrapperFactory;
 import com.seibel.lod.core.wrapperInterfaces.chunk.AbstractChunkPosWrapper;
+import com.seibel.lod.core.wrapperInterfaces.minecraft.IMinecraftRenderWrapper;
 import com.seibel.lod.core.wrapperInterfaces.modAccessor.ISodiumAccessor;
 
 
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.SectionPos;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelHeightAccessor;
 
 public class SodiumAccessor implements ISodiumAccessor {
-    IWrapperFactory factory = SingletonHandler.get(IWrapperFactory.class);
+	private final IMinecraftRenderWrapper MC_RENDER = SingletonHandler.get(IMinecraftRenderWrapper.class);
 
     @Override
     public String getModName() {
-        return "Sodium-Fabric-1.17.1";
-    }
+    	return "Sodium-Fabric-1.17.1";
+	}
 
-    @Override
-    public HashSet<AbstractChunkPosWrapper> getNormalRenderedChunks() {
-//        SodiumWorldRenderer renderer = SodiumWorldRenderer.instance();
-//        LevelHeightAccessor height =  Minecraft.getInstance().level;
-//        // 0b11 = Lighted chunk & loaded chunk
-//        return renderer.getChunkTracker().getChunks(0b11).filter(
-//                (long l) -> {
-//                    for (int i = height.getMinSection(); i<height.getMaxSection(); i++) {
-//                        SectionPos p = SectionPos.of(new ChunkPos(l), i);
-//                        if (renderer.isBoxVisible(p.minBlockX()+1, p.minBlockY()+1, p.minBlockZ()+1,
-//                                p.maxBlockX()-1, p.maxBlockY()-1, p.maxBlockZ()-1)) return true;
-//                    }
-//                    return false;
-//                }).mapToObj((long l) -> {
-//            return (AbstractChunkPosWrapper)factory.createChunkPos(l);
-//        }).collect(Collectors.toCollection(HashSet::new));
-        return null;
-    }
-
+	@Override
+	public HashSet<AbstractChunkPosWrapper> getNormalRenderedChunks() {
+		SodiumWorldRenderer renderer = SodiumWorldRenderer.instance();
+		LevelHeightAccessor height = Minecraft.getInstance().level;
+		
+		// TODO: Maybe use a mixin to make this more efficient
+		return MC_RENDER.getMaximumRenderedChunks().stream().filter((AbstractChunkPosWrapper chunk) -> {
+			return (renderer.isBoxVisible(
+					chunk.getMinBlockX()+1, height.getMinBuildHeight()+1, chunk.getMinBlockZ()+1,
+					chunk.getMinBlockX()+15, height.getMaxBuildHeight()-1, chunk.getMinBlockZ()+15));
+		}).collect(Collectors.toCollection(HashSet::new));
+	}
 }
