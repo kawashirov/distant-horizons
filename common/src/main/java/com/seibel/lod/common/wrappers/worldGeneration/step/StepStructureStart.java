1222db45d64c032307f0d874c5e4f349bb3ff8b7
tom lee <tomlee92502@yahoo.com>
2022-02-05 16:17:31 +0800

    Update core + remove duped createStructureStart() call.

--- common/src/main/java/com/seibel/lod/common/wrappers/worldGeneration/step/StepStructureStart.java
+++ common/src/main/java/com/seibel/lod/common/wrappers/worldGeneration/step/StepStructureStart.java
@@ -42,17 +42,6 @@ public final class StepStructureStart {
 				// System.out.println("StepStructureStart: "+chunk.getPos());
 				envionment.params.generator.createStructures(envionment.params.registry, tParams.structFeat, chunk,
 						envionment.params.structures, envionment.params.worldSeed);
-				// try {
-				envionment.params.generator.createStructures(envionment.params.registry, tParams.structFeat, chunk,
-						envionment.params.structures, envionment.params.worldSeed);
-				// tParams.structCheck.onStructureLoad(chunk.getPos(), chunk.getAllStarts());
-				/*
-				 * } catch (ArrayIndexOutOfBoundsException e) { // There's a rare issue with
-				 * StructStart where it throws ArrayIndexOutOfBounds // This means the
-				 * structFeat is corrupted (For some reason) and I need to reset it. // TODO:
-				 * Figure out in the future why this happens even though I am using new
-				 * structFeat throw new StructStartCorruptedException(e); }
-				 */
 			}
 		}
 	}
