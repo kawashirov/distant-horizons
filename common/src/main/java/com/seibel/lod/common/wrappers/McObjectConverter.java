03deddf6661903e0163978da26e2ac22deeb842d
tom lee <tomlee92502@yahoo.com>
2022-02-26 14:04:49 +0800

    Optimized the McObjectConverter for Direction

--- common/src/main/java/com/seibel/lod/common/wrappers/McObjectConverter.java
+++ common/src/main/java/com/seibel/lod/common/wrappers/McObjectConverter.java
@@ -47,8 +47,18 @@ public class McObjectConverter
     }
 
 
+    static final Direction[] directions;
+    static {
+    	LodDirection[] lodDirs = LodDirection.values();
+    	directions = new Direction[lodDirs.length];
+    	for (LodDirection lodDir : lodDirs) {
+    		directions[lodDir.ordinal()] = Direction.byName(lodDir.name());
+    	}
+    }
+    
+    
     public static Direction Convert(LodDirection lodDirection)
     {
-        return Direction.byName(lodDirection.name());
+        return directions[lodDirection.ordinal()];
     }
 }
