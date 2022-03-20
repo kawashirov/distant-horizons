17e375bc61a1855a6a1dccaad6ba145e0a7454ee
James Seibel <jeseibel@gondtc.com>
2022-03-19 20:04:11 -0500

    Return IBlockDetailWrappers

--- common/src/main/java/com/seibel/lod/common/wrappers/chunk/ChunkWrapper.java
+++ common/src/main/java/com/seibel/lod/common/wrappers/chunk/ChunkWrapper.java
@@ -1,14 +1,15 @@
 package com.seibel.lod.common.wrappers.chunk;
 
+import com.seibel.lod.common.wrappers.block.BlockDetailWrapper;
 import com.seibel.lod.core.enums.LodDirection;
 import com.seibel.lod.core.util.LevelPosUtil;
 import com.seibel.lod.core.util.LodUtil;
+import com.seibel.lod.core.wrapperInterfaces.block.IBlockDetailWrapper;
 import com.seibel.lod.core.wrapperInterfaces.chunk.IChunkWrapper;
 import com.seibel.lod.core.wrapperInterfaces.world.IBiomeWrapper;
 
 import com.seibel.lod.common.wrappers.WrapperUtil;
 import com.seibel.lod.common.wrappers.block.BlockDetailMap;
-import com.seibel.lod.common.wrappers.block.BlockDetailWrapper;
 import com.seibel.lod.common.wrappers.world.BiomeWrapper;
 import com.seibel.lod.common.wrappers.worldGeneration.mimicObject.LightedWorldGenRegion;
 
@@ -77,15 +78,15 @@ public class ChunkWrapper implements IChunkWrapper
 	}
 	
 	@Override
-	public BlockDetailWrapper getBlockDetail(int x, int y, int z) {
+	public IBlockDetailWrapper getBlockDetail(int x, int y, int z) {
 		BlockPos pos = new BlockPos(x,y,z);
 		BlockState blockState = chunk.getBlockState(pos);
-		BlockDetailWrapper blockDetail = BlockDetailMap.getOrMakeBlockDetailCache(blockState, pos, lightSource);
+		IBlockDetailWrapper blockDetail = BlockDetailMap.getOrMakeBlockDetailCache(blockState, pos, lightSource);
 		return blockDetail == BlockDetailWrapper.NULL_BLOCK_DETAIL ? null : blockDetail;
 	}
 
     @Override
-    public BlockDetailWrapper getBlockDetailAtFace(int x, int y, int z, LodDirection dir) {
+    public IBlockDetailWrapper getBlockDetailAtFace(int x, int y, int z, LodDirection dir) {
         int fy = y+dir.getNormal().y;
         if (fy < getMinBuildHeight() || fy > getMaxBuildHeight()) return null;
         BlockPos pos = new BlockPos(x+dir.getNormal().x,fy,z+dir.getNormal().z);
@@ -96,7 +97,7 @@ public class ChunkWrapper implements IChunkWrapper
             blockState = lightSource.getBlockState(pos);
         }
         if (blockState == null || blockState.isAir()) return null;
-        BlockDetailWrapper blockDetail = BlockDetailMap.getOrMakeBlockDetailCache(blockState, pos, lightSource);
+		IBlockDetailWrapper blockDetail = BlockDetailMap.getOrMakeBlockDetailCache(blockState, pos, lightSource);
         return blockDetail == BlockDetailWrapper.NULL_BLOCK_DETAIL ? null : blockDetail;
     }
 	
