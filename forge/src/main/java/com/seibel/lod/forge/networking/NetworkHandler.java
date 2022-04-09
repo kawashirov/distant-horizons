502008466385890f66b92b2ea4ec7d7c6ebb2ec0
James Seibel <jeseibel@gondtc.com>
2022-04-08 21:07:12 -0500

    Remove the fabric networking code
    The code isn't currently in use and is causing issues in 1.16.5.
    
    I put the code in zip files in case someone wants to quickly re-add it.

--- forge/src/main/java/com/seibel/lod/forge/networking/NetworkHandler.java
+++ forge/src/main/java/com/seibel/lod/forge/networking/NetworkHandler.java
@@ -21,8 +21,8 @@ package com.seibel.lod.forge.networking;
 
 import com.seibel.lod.common.networking.NetworkInterface;
 import com.seibel.lod.common.networking.Networking;
-import com.seibel.lod.forge.fabric.api.client.networking.v1.ClientPlayNetworking;
-import com.seibel.lod.forge.fabric.api.networking.v1.ServerPlayNetworking;
+//import com.seibel.lod.forge.fabric.api.client.networking.v1.ClientPlayNetworking;
+//import com.seibel.lod.forge.fabric.api.networking.v1.ServerPlayNetworking;
 
 /**
  * @author Ran
@@ -30,15 +30,15 @@ import com.seibel.lod.forge.fabric.api.networking.v1.ServerPlayNetworking;
 public class NetworkHandler implements NetworkInterface {
     @Override
     public void register_Client() {
-        ClientPlayNetworking.registerGlobalReceiver(Networking.resourceLocation_meow, (client, handler, buf, responseSender) -> {
-            com.seibel.lod.common.networking.NetworkHandler.receivePacketClient(client, handler, buf);
-        });
+//        ClientPlayNetworking.registerGlobalReceiver(Networking.resourceLocation_meow, (client, handler, buf, responseSender) -> {
+//            com.seibel.lod.common.networking.NetworkHandler.receivePacketClient(client, handler, buf);
+//        });
     }
 
     @Override
     public void register_Server() {
-        ServerPlayNetworking.registerGlobalReceiver(Networking.resourceLocation_meow, (server, player, handler, buf, responseSender) -> {
-            com.seibel.lod.common.networking.NetworkHandler.receivePacketServer(server, player, handler, buf);
-        });
+//        ServerPlayNetworking.registerGlobalReceiver(Networking.resourceLocation_meow, (server, player, handler, buf, responseSender) -> {
+//            com.seibel.lod.common.networking.NetworkHandler.receivePacketServer(server, player, handler, buf);
+//        });
     }
 }
