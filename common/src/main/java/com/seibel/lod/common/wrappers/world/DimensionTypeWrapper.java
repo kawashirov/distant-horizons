d22aab08cb272aa3cc4b2ec9e0d8bcc0b3f6ed50
James Seibel <jeseibel@gondtc.com>
2022-04-24 15:29:03 -0500

    Change the license from GPL to LGPL

--- common/src/main/java/com/seibel/lod/common/wrappers/world/DimensionTypeWrapper.java
+++ common/src/main/java/com/seibel/lod/common/wrappers/world/DimensionTypeWrapper.java
@@ -1,19 +1,19 @@
 /*
  *    This file is part of the Distant Horizons mod (formerly the LOD Mod),
- *    licensed under the GNU GPL v3 License.
+ *    licensed under the GNU LGPL v3 License.
  *
  *    Copyright (C) 2020-2022  James Seibel
  *
  *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
+ *    it under the terms of the GNU Lesser General Public License as published by
  *    the Free Software Foundation, version 3.
  *
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *    GNU Lesser General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
+ *    You should have received a copy of the GNU Lesser General Public License
  *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
