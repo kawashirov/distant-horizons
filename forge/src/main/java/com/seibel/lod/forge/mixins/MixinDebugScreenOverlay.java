3e71f31f8e29a90935ce26f31c3b24a8263fcc02
coolGi2007 <sasanaps@hotmail.com>
2022-05-26 08:58:10 +0000

    Added some basic stuff for the f3 screen

---
+++ forge/src/main/java/com/seibel/lod/forge/mixins/MixinDebugScreenOverlay.java
@@ -0,0 +1,24 @@
+package com.seibel.lod.forge.mixins;
+
+import com.seibel.lod.core.render.F3Screen;
+import net.minecraft.client.gui.components.DebugScreenOverlay;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(DebugScreenOverlay.class)
+public class MixinDebugScreenOverlay {
+
+    @Inject(method = "getSystemInformation", at = @At("RETURN"))
+    private void addCustomF3(CallbackInfoReturnable<List<String>> cir) {
+        List<String> messages = cir.getReturnValue();
+
+        for (String i: F3Screen.f3List) {
+            messages.add(i);
+        }
+    }
+
+}
