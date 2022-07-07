b7254c7d12bbd64f7640c3cddf91256e8d304e24
coolGi <sasanaps@hotmail.com>
2022-07-07 14:22:52 +0000

    Fixed windows auto builder script

--- buildAll.bat
+++ buildAll.bat
@@ -1,16 +1,17 @@
 @echo off
 
-:buildVersion
-call gradlew.bat -PmcVer=%~1 build
-call gradlew.bat mergeJars -PmcVer=%~1
-rm -R build/libs common/build/libs fabric/build/libs forge/build/libs
-EXIT /B 0
-
 SETLOCAL
-CALL :buildVersion 1.19
-CALL :buildVersion 1.18.2
-CALL :buildVersion 1.18.1
-CALL :buildVersion 1.17.1
-CALL :buildVersion 1.16.5
+CALL :buildVersion "1.19"
+CALL :buildVersion "1.18.2"
+CALL :buildVersion "1.18.1"
+CALL :buildVersion "1.17.1"
+CALL :buildVersion "1.16.5"
 EXIT /B %ERRORLEVEL%
 
+:buildVersion
+@echo on
+call ./gradlew.bat clean -PmcVer="%~1" --no-daemon
+call ./gradlew.bat build -PmcVer="%~1" --no-daemon
+call ./gradlew.bat mergeJars -PmcVer="%~1" --no-daemon
+@echo off
+EXIT /B 0
