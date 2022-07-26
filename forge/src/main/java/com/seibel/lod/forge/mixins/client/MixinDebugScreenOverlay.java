a4ae25886aa1852009685839bc28fb543354b42a
TomTheFurry <tomlee92502@yahoo.com>
2022-07-26 20:17:26 +0800

    Continue fixing bugs... Lots of bugs.

--- forge/src/main/java/com/seibel/lod/forge/mixins/client/MixinDebugScreenOverlay.java
+++ forge/src/main/java/com/seibel/lod/forge/mixins/client/MixinDebugScreenOverlay.java
@@ -1,6 +1,6 @@
 package com.seibel.lod.forge.mixins.client;
 
-import com.seibel.lod.core.render.F3Screen;
+import com.seibel.lod.core.logging.f3.F3Screen;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +15,7 @@ public class MixinDebugScreenOverlay {
     @Inject(method = "getSystemInformation", at = @At("RETURN"))
     private void addCustomF3(CallbackInfoReturnable<List<String>> cir) {
         List<String> messages = cir.getReturnValue();
-
-        for (String i: F3Screen.f3List) {
-            messages.add(i);
-        }
+        F3Screen.addStringToDisplay(messages);
     }
 
 }
