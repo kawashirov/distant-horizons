7624764ee07359f45bdc82c5eb611ec366367571
TomTheFurry <tomlee92502@yahoo.com>
2023-06-15 16:41:18 +0800

    Fix lighting issues in both client-only and single player world

--- fabric/src/main/java/com/seibel/lod/fabric/mixins/client/MixinLevelRenderer.java
+++ fabric/src/main/java/com/seibel/lod/fabric/mixins/client/MixinLevelRenderer.java
@@ -21,15 +21,18 @@ package com.seibel.lod.fabric.mixins.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
+import com.seibel.lod.common.wrappers.chunk.ChunkWrapper;
 import com.seibel.lod.core.config.Config;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.lighting.LevelLightEngine;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -112,4 +115,24 @@ public class MixinLevelRenderer
         }
     }
 	#endif
+
+    @Redirect(method =
+                "Lnet/minecraft/client/renderer/LevelRenderer;" +
+                "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;" +
+                "FJZLnet/minecraft/client/Camera;" +
+                "Lnet/minecraft/client/renderer/GameRenderer;" +
+                "Lnet/minecraft/client/renderer/LightTexture;" +
+                "Lcom/mojang/math/Matrix4f;)V"
+            ,
+            at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/lighting/LevelLightEngine;runUpdates(IZZ)I"
+            ))
+    private int callAfterRunUpdates(LevelLightEngine light, int pos, boolean isQueueEmpty, boolean updateBlockLight)
+    {
+        int r = light.runUpdates(pos, isQueueEmpty, updateBlockLight);
+        ChunkWrapper.syncedUpdateClientLightStatus();
+        return r;
+    }
+
 }
