b41119232e42a4a96da8cfdb4c826ba1841b7221
James Seibel <jeseibel@gondtc.com>
2023-08-05 17:27:30 -0500

    Remove BCLibAccessor from 1.16

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/wrappers/modAccessor/BCLibAccessor.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/wrappers/modAccessor/BCLibAccessor.java
@@ -1,7 +1,8 @@
 package com.seibel.distanthorizons.fabric.wrappers.modAccessor;
 
 import com.seibel.distanthorizons.core.wrapperInterfaces.modAccessor.IBCLibAccessor;
-#if PRE_MC_1_19_2
+#if MC_1_16_5
+#elif PRE_MC_1_19_2
 import ru.bclib.config.ClientConfig;
 import ru.bclib.config.Configs;
 #else
@@ -9,15 +10,18 @@ import org.betterx.bclib.config.ClientConfig;
 import org.betterx.bclib.config.Configs;
 #endif
 
-public class BCLibAccessor implements IBCLibAccessor {
+public class BCLibAccessor implements IBCLibAccessor
+{
     @Override
-    public String getModName() {
-        return "BCLib";
-    }
+    public String getModName() { return "BCLib"; }
 
-    public void setRenderCustomFog(boolean newValue) {
-        // Change the value of CUSTOM_FOG_RENDERING in the bclib client config
+    public void setRenderCustomFog(boolean newValue)
+    {
+	    #if MC_1_16_5
+	    #elif PRE_MC_1_19_2
+	    // Change the value of CUSTOM_FOG_RENDERING in the bclib client config
         // This disabled fog from rendering within bclib
         Configs.CLIENT_CONFIG.set(ClientConfig.CUSTOM_FOG_RENDERING, newValue);
+		#endif
     }
 }
