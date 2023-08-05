a52a122f9fcbddbfc418f4cee4fbce4d743c4519
James Seibel <jeseibel@gondtc.com>
2023-08-05 18:17:01 -0500

    Update MixinThreadingDetector.java

--- forge/src/main/java/com/seibel/distanthorizons/forge/mixins/server/unsafe/MixinThreadingDetector.java
+++ forge/src/main/java/com/seibel/distanthorizons/forge/mixins/server/unsafe/MixinThreadingDetector.java
@@ -32,7 +32,7 @@ import java.util.concurrent.Semaphore;
 
 /**
  * Why does this exist? But okay! (Will be probably removed when the experimental generator is done)
- * FIXME: Recheck this
+ * FIXME: Recheck this // STILL check this
  */
 @Mixin(ThreadingDetector.class)
 public class MixinThreadingDetector {
@@ -49,5 +49,7 @@ public class MixinThreadingDetector {
 #else
 
 import net.minecraft.world.level.chunk.ChunkGenerator;
+
 @Mixin(ChunkGenerator.class)
+public class MixinThreadingDetector { }
 #endif
