521549a34928f68ac50c5fc77bc374fd0f3bf93e
James Seibel <jeseibel@gondtc.com>
2023-08-06 15:37:55 -0500

    Fix MixinTextureUtil mixin crash due to incorrect package path

--- forge/src/main/java/com/seibel/distanthorizons/forge/mixins/client/MixinTextureUtil.java
+++ forge/src/main/java/com/seibel/distanthorizons/forge/mixins/client/MixinTextureUtil.java
@@ -13,13 +13,15 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  * @author coolGi
  */
 @Mixin(TextureUtil.class)
-public class MixinTextureUtil {
-    @Redirect(method = "Lcom/mojang/blaze3d/platform/TextureUtil;prepareImage(Lcom/mojang/blaze3d/platform/NativeImage$InternalGlFormat;IIII)V",
+public class MixinTextureUtil
+{
+    @Redirect(method = "prepareImage(Lcom/mojang/blaze3d/platform/NativeImage$InternalGlFormat;IIII)V",
             at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;_texParameter(IIF)V"), remap=false)
     private static void setLodBias(int target, int pname, float param)
 	{
         float biasValue = Config.Client.Advanced.Graphics.AdvancedGraphics.lodBias.get().floatValue();
-        if (biasValue != 0) {
+        if (biasValue != 0)
+		{
             // The target is GL11.GL_TEXTURE_2D
             // And the pname is GL14.GL_TEXTURE_LOD_BIAS
             GlStateManager._texParameter(target, pname, biasValue);
