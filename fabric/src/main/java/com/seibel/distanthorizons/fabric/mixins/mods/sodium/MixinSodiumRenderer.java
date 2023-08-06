8235a911ab60788d14c1237ed0ef478a3a919b9a
coolGi <sasanaps@hotmail.com>
2023-08-06 22:18:48 +0930

    Fixed sodium not working on post 1.16

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/mods/sodium/MixinSodiumRenderer.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/mods/sodium/MixinSodiumRenderer.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SodiumWorldRenderer.class)
+@Mixin(RegionChunkRenderer.class)
 public class MixinSodiumRenderer 
 {
     @Unique SodiumAccessor accessor = null;
