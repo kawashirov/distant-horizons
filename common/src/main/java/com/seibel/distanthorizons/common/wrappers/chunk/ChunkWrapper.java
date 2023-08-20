e1147f3f6089208f9c31a21681506370f03ec8c2
James Seibel <jeseibel@gondtc.com>
2023-08-20 17:07:30 -0500

    Replace ChunkWrapper LinkedList with ArrayList

--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/chunk/ChunkWrapper.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/chunk/ChunkWrapper.java
@@ -71,7 +71,7 @@ public class ChunkWrapper implements IChunkWrapper
 	private final byte[] blockLightArray;
 	private final byte[] skyLightArray;
 	
-	private LinkedList<DhBlockPos> blockLightPosList = null;
+	private ArrayList<DhBlockPos> blockLightPosList = null;
 	
 	private boolean useDhLighting;
 	
@@ -314,7 +314,7 @@ public class ChunkWrapper implements IChunkWrapper
 		// only populate the list once
 		if (this.blockLightPosList == null)
 		{
-			this.blockLightPosList = new LinkedList<>();
+			this.blockLightPosList = new ArrayList<>();
 			
 			
 			#if PRE_MC_1_20_1
