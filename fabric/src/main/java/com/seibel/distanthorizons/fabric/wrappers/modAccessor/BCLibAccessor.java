37cbeebaa5c06c8762adeb20ddf28749d216139d
James Seibel <jeseibel@gondtc.com>
2023-08-25 18:26:52 -0500

    Fix BCLib compiling for MC 1.16 and 1.17

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/wrappers/modAccessor/BCLibAccessor.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/wrappers/modAccessor/BCLibAccessor.java
@@ -1,10 +1,8 @@
 package com.seibel.distanthorizons.fabric.wrappers.modAccessor;
 
 import com.seibel.distanthorizons.core.wrapperInterfaces.modAccessor.IBCLibAccessor;
-#if MC_1_16_5
-import org.betterx.bclib.config.Configs;
-import org.betterx.bclib.config.ClientConfig;
-#elif MC_1_17_1 || MC_1_18_2
+#if MC_1_16_5 || MC_1_17_1
+#elif MC_1_18_2
 import ru.bclib.config.ClientConfig;
 import ru.bclib.config.Configs;
 #else
@@ -19,7 +17,7 @@ public class BCLibAccessor implements IBCLibAccessor
 	
 	public void setRenderCustomFog(boolean newValue)
 	{
-		#if !MC_1_17_1 // 1.17 doesn't have "ClientConfig.CUSTOM_FOG_RENDERING"
+		#if !(MC_1_16_5 || MC_1_17_1) // 1.16 and 1.17 don't have "ClientConfig.CUSTOM_FOG_RENDERING"
 		
 		// Change the value of CUSTOM_FOG_RENDERING in the bclib client config
 		// This disabled fog from rendering within bclib
