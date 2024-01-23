de8005843f5fb24e9e9f735152afb0ceb44c58ac
kawa <1196609+kawashirov@users.noreply.github.com>
2024-01-23 10:02:10 +0700

    2.0.1-a-kawa-4

--- buildForMiA.cmd
+++ buildForMiA.cmd
@@ -2,13 +2,7 @@
 
 mkdir buildForMiA
 
-set version="1.20.1"
-call .\gradlew.bat clean --no-daemon
-call .\gradlew.bat build -PmcVer="!version!" --no-daemon
-call .\gradlew.bat mergeJars -PmcVer="!version!" --no-daemon
-move Merged\*.jar buildForMiA\
-
-set version="1.20.2"
+set version="1.20.4"
 call .\gradlew.bat clean --no-daemon
 call .\gradlew.bat build -PmcVer="!version!" --no-daemon
 call .\gradlew.bat mergeJars -PmcVer="!version!" --no-daemon
